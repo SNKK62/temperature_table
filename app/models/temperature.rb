a6a7c25d26d6ab78bf19a5be5871d9e50c9e31e6
SNKK62 <78491085+SNKK62@users.noreply.github.com>
2021-08-21 18:47:08 +0900

    Update temperature.rb

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -7,6 +7,9 @@ class Temperature < ApplicationRecord
     def self.add_temps
         @users = User.all
         @users.each do |user|
+           if temp.num == "未記入"
+            temp.destroy
+          end
           user.temperatures.create(num: "未記入")
         end
       end
